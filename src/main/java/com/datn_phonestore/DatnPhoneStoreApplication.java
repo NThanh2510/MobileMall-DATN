1d6d007d1e588e0ebecccf15830f22e863b7b62a
NThanh2510 <cainhatthanhwork@gmail.com>
2025-01-17 20:05:27 +0700

    init commit

---
+++ src/main/java/com/datn_phonestore/DatnPhoneStoreApplication.java
@@ -0,0 +1,13 @@
+package com.datn_phonestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnPhoneStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatnPhoneStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/datn_phonestore/DatnPhoneStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datn_phonestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatnPhoneStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
